c29805e9783f41f0cccb6b4e6461c367ba51d07b
MorrisB-- <morris.ballenger@yahoo.com>
2016-08-04 17:26:16 -0500

    Added idea for run.java

--- src/Run.java
+++ src/Run.java
@@ -1,27 +1,18 @@
 public class Run {
 
+	@SuppressWarnings("static-access")
 	public static void main(String[] args) {
 
 		WumpusBoard wb = new WumpusBoard();
 		wb.InitializeBoard();
 		wb.PrintBoard();
 		
-		wb.Algorithm();
-		wb.PrintBoard();
-
-		wb.Algorithm();
-		wb.PrintBoard();
-		
-		wb.Algorithm();
-		wb.PrintBoard();
-		
-		wb.Algorithm();
-		wb.PrintBoard();
-		
-		wb.Algorithm();
-		wb.PrintBoard();
-		
-		wb.Algorithm();
-		wb.PrintBoard();
+		int moves =0;
+		while(wb.Algorithm(moves)){
+			moves++;
+			if (moves > 9)
+				moves = 0;
+			wb.PrintBoard();
+		}
 	}
 }
